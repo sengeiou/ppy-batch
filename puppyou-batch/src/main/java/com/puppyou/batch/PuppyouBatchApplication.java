c07069c21ab74e1a9a6e4828b787683be7d86a32
gusrn1221@shstechnology.com <gusrn1221@shstechnology.com>
2021-03-25 18:12:23 +0900

    배치 타임존 변경

--- puppyou-batch/src/main/java/com/puppyou/batch/PuppyouBatchApplication.java
+++ puppyou-batch/src/main/java/com/puppyou/batch/PuppyouBatchApplication.java
@@ -1,10 +1,19 @@
 package com.puppyou.batch;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PuppyouBatchApplication {
+	
+	@PostConstruct
+	void startedTime() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PuppyouBatchApplication.class, args);
